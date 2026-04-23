37222f0c0a3e956d05fd669441f7fe2584694d0d
qovery <github@qovery.com>
2026-04-23 12:06:15 +0000

    See https://github.com/Qovery/qovery-openapi-spec/commit/acd86efe0c0399faf73232ac05739f240bd8554c from refs/heads/main

--- docs/LinkedServiceTypeEnum.md
+++ docs/LinkedServiceTypeEnum.md
@@ -10,7 +10,6 @@
 | Job | JOB |
 | Helm | HELM |
 | Terraform | TERRAFORM |
-| ArgocdApp | ARGOCD_APP |
 
 
 [[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
--- src/models/service_type_enum.rs
+++ src/models/service_type_enum.rs
@@ -27,6 +27,8 @@ pub enum ServiceTypeEnum {
     Helm,
     #[serde(rename = "TERRAFORM")]
     Terraform,
+    #[serde(rename = "ARGOCD_APP")]
+    ArgocdApp,
 }
 
 impl std::fmt::Display for ServiceTypeEnum {
@@ -38,6 +40,7 @@ impl std::fmt::Display for ServiceTypeEnum {
             Self::Job => write!(f, "JOB"),
             Self::Helm => write!(f, "HELM"),
             Self::Terraform => write!(f, "TERRAFORM"),
+            Self::ArgocdApp => write!(f, "ARGOCD_APP"),
         }
     }
 }
